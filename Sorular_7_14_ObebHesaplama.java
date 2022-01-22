f1815ab29006dbcc8a0e205b5bb87772d38cde53
muratkorkun <75913102+muratkorkun@users.noreply.github.com>
2022-01-22 14:32:03 +0300

    kod ve yorum düzeltmeleri
    kod ve yorumlar daha anlaşılabilir hale getirilmeye çalışıldı.

--- Sorular_7_14_ObebHesaplama.java
+++ Sorular_7_14_ObebHesaplama.java
@@ -22,24 +22,36 @@ public class Sorular_7_14_ObebHesaplama {
 
 	public static int obebHesapla(int... numbers) {
 		int obeb = 1; // obebi 1 den baþlattýk.
-		int minDeger = min(numbers); // listedeki en küçük degeri belirledik.
-		boolean bolenMi; // Dizinin elemanlarýný bölebilen bir deðer var mý ? Kontrol ediyoruz.
+		int minDeger = min(numbers); // listedeki en küçük degeri belirledik. Obeb en küçük 1 en büyük min deðeri
+										// olabileeðinden yola çýkarak aðaþýdaki algoritmayý yazdýk.
+		boolean hepsiniBoluyorMu = true; // Dizinin her elemanýný tektek i sayýsýna bölünebiliyor mu ? Kontrol ediyoruz.
 
-		// 2 ile girilen minumum deðere kadar olan deðerler arasýnda geçiþ yapýyoruz.
-		for (int i = 2; i <= minDeger; i++) {
-		
-			bolenMi = true; // bu i degerlerini en baþta bölen olarak kabul ediyoruz.
-			for (int j : numbers) { // artan bölen deðeri ile ( yani i ile ) dizideki tüm elemanlarýn kalansýz
-									// olarak bölünüp bölünmediðini kontrol ediyoruz.
-				if (j % i != 0)
-					bolenMi = false;// bölünemiyorsa false atamasý yapýyoruz.
+		for (int i = 2; i <= minDeger;i++) { // Obebi bulabilmek için bölen sayýyýnýn (i) bölünen her sayýyý(number) tek
+											// tek kontrol etmesi gerekiyor. Bu nedenle i deðerini sabit tutarak dizi
+											// içindeki bütün sayýlarda i'nin dizideki sayýyý bölüp bölmediðini
+											// kýyaslýyoruz.
+
+			for (int number : numbers) { // Bu döngüde i için sayýlarý tek tek kontrol ettiðimizde þartýmýza uygun
+											// þekilde bir i deðeri bulunduysa bu deðeri alýp obeb olarak kullanýyoruz.
+											// Ta ki i yi min deðerine kadar kontrol ederek bunu yapýyoruz.
+				if (number % i == 0) {
+					hepsiniBoluyorMu = true; // i sayýyý bölüyorsa boolean kontrol deðerimize true atamasý yapýyoruz.
+												// Döngü sonunda her sayý i'ye bölünüyorsa deðerimiz true olarak
+												// kalacaktýr.
+				} else {
+					hepsiniBoluyorMu = false; // Aksi halde bir tane sayý bile i'ye tam bölünmüyorsa kontrol deðerimize
+												// false atamasý yapýyoruz.
+					break;
+				}
+
 			}
-
-			if (bolenMi == true) // bölünüyorsa ve yeni bölen bulunuyorsa bölen deðerini güncelliyoruz.
-				obeb = i;
+			if (hepsiniBoluyorMu == true) {
+				obeb = i; // i deðeri min deðerine kadar kontrol edileceðinden ortak bölenlerin en
+							// büyüðünü (obeb) bulmuþ olacaðýz.
+			}
 		}
-
-		return obeb;
+		return obeb; // obeb 1 ve min dahil aralýðýndan algoritmamýza uygun herhangi bir sayý
+						// olabilecektir.
 	}
 
 	// Bir dizi içindeki minimum deðeri belirleme metodu.
@@ -52,4 +64,4 @@ public class Sorular_7_14_ObebHesaplama {
 		return min;
 	}
 
-}
+}  // dizi Örneklemleri :  6 12 21 9  15  ,   18 12 36 360 480   ,  3 15 48 96 129
