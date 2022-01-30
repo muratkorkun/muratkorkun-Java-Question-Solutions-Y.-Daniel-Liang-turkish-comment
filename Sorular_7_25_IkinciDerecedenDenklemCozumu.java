9cc5fb2ee25b7c10dd7b4ed176e75e13cd289ad2
muratkorkun <75913102+muratkorkun@users.noreply.github.com>
2022-01-30 13:17:50 +0300

    EX_7.25_Algebra:_solve_quadratic_equations
    Türkçe daha açıklayıcı yorumlar eklendi ve printf komutu hakkında açıklama yapıldı.

--- Sorular_7_25_IkinciDerecedenDenklemCozumu.java
+++ Sorular_7_25_IkinciDerecedenDenklemCozumu.java
@@ -2,42 +2,54 @@ package Bolum07_DersteYazilanKodlar;
 
 import java.util.Scanner;
 
-public class Sorular_7_25_IkinciDerecedenDenklemCozumu {
-
+public class Sorular_7_25_IkinciDerecedenDenklemCozumu {   // Þimdi bu soruda öncelikle neden methoda deðerleri hesaplanmamýþ olan kökler dizisi parametresini istediðini anlayamadým. Yani kökleri belli olmadan neden kökler dizisini almamýz gerekiyor ? 
+														//Bu sorunun cevabý sorunun þudur : aslýnda bir method ile bir diziyi alýp tek bir integer deðer bile döndürsen bu method içinde aldýðýmýz dizilere yaptýðýmýz deðiþiklikler uygulanýr.
+														// Yani methoda parametre olarak alýnan dizinin kendisi(referansý) deðiþmez fakat içeriði deðiþebilir.Bu deðiþen içeriði dönderdiðimiz int deðer ile baðdaþtýrarak diziyi yazdýrabiliriz.
+														//Bu soru çok zorla(ma) üretilmiþ bir soru olmuþ ama yinede bir programcýlýk öðrencisine, öðretmek istedikleri çok önemli kavramlara deðinilmiþ.
 	public static void main(String[] args) {
 
-		System.out.println("Köklerin  hesabýnýn yapýlmasý için a,b ,c  deðerleri giriniz.");
+		System.out.println("Denklem köklerin hesabýnýn yapýlmasý için a,b ,c deðerleri giriniz.");
 		Scanner klavye = new Scanner(System.in);
-		double[] sabitler = new double[3];
-		double[] kokler = new double[2];
+		double[] sayilar = new double[3];       // Burada öncelikle a, b, c deðerlerini tutacaðýmýz diziyi oluþturduk.
+		double[] kokler = new double[2];		// Burada methoda parametre olarak göndereceðimiz diziyi oluþturduk. Ýlklendirme sonucunda dizinin tüm elemanlarý '0'ile dolduruldu.
 
-		for (int i = 0; i < sabitler.length; i++) {
-			sabitler[i] = klavye.nextDouble();
+		for (int i = 0; i < sayilar.length; i++) {    // Burada Scanner ile kullanýcýdan hesaplanacak olan sayýlarý alýp diziye doldurduk.
+			sayilar[i] = klavye.nextDouble();
 		}
 
-		klavye.close();
+		klavye.close();							// Scanner i kapattýk.
 
-		int kokSayisi = cozIkinciDereceden(sabitler, kokler);
+		int kokSayisi = cozIkinciDereceden(sayilar, kokler);			//Method int deðer döndüðü için dönen deðeri int bir deðiþkene atadýk.Methoda kök sayýsýnýn ve köklerin hesaplanmasý için sayilar dizisini gönderdik.
+																		//Yine methoda kökler parametresini gönderip method içinde bulunan kök sayýsý ile ilintili þekilde kökler dizisini doldurduk.
+																		//Yani parametre olarak referansýný aldýðýmýz kökler dizisinin içeriðini deðiþtirdik.Daha sonra devam eden kod satýrýmýzda bu deðiþen kökleri kök sayýsýna uygun olacak þekilde yazdýrdýk. 
 
-		if (kokSayisi == 0)
-			System.out.println("Hiçbir kök yoktur");
-		else
-			for (int i = 0; i < kokSayisi; i++) {
-				System.out.print("Kök " + (i + 1) + ": " + kokler[i] + "\n");
+		if (kokSayisi == 0)													//Köklerin sayýsý 0 ise gereken þekilde yazdýrdýk.
+			System.out.println("Denklemde hiçbir kök yoktur.");
+		else					
+			System.out.printf("Denklemin %d tane gerçek kökü vardýr.\n",kokSayisi);
+			for (int i = 0; i < kokSayisi; i++) {								// köklerin sayýsý kadar döngü ile kök numarasýný ve kök deðerlerini ekrana yazdýrdýk.
+				System.out.printf("Kök %d: %.6f\n",(i + 1),kokler[i]);			// Burada yeri gelmiþken printf komututun nasýl çalýþtýðýndan bahsedelim. 
+									//Temel olarak String içerisinde yazdýrmamýz gereken her bir deðiþken için yer tutucu olarak tabir edilen % ile baþlýyan yer tutucularý deðiþkenin türüne uygun þekilde String içinde kullanýyoruz.
+									//Yer tutuculara karþýlýk gelen deðiþkenleri de, String sonuna ve deðiþken aralarýna bir virgül olacak þekilde sýra ile yazýyoruz.
+									//Yani yer tutucular String içerisinde herbir deðiþkene karþýlýk tek tek yazýlýr ve yer tutucuya karþýlýk gelen bütün deðiþkenler String sonunda tek tek virgül ile ayrýlarak yazýlýr.
+									//Ayný zamanda yer tutucularýn saðýnda solunda istediðiniz þekilde String yazýmýna devam edebilirsiniz. 
+									//Yazýlan komutlar sonunda print komutu çalýþtýðýnda bütün deðiþkenler yer tutucularýn yerine ekranda kendini gösterir.
 			}
 	}
 
-	public static int cozIkinciDereceden(double[] sabitler, double[] kokler) {
-
-		double a = sabitler[0];
-		double b = sabitler[1];
-		double c = sabitler[2];
+	public static int cozIkinciDereceden(double[] sayilar, double[] kokler) {  
+						// Bu method parametre olarak iki farklý dizi alýr, referansýný aldýðý dizilerden birinin elemanlarýný tek tek iþlem yapmak üzere primitive deðiþkenlere kopyalar ve iþlemlerde kullanýr. Not: Referansýnda deðiþiklik yapmaz öylece býrakýr.
+												//Referansýný aldýðý diðer dizinin iþlemler sonucuna göre içeriðini deðiþtirir. Not : Referansýnda deðiþiklik yapar.
+												//Yine iþlemler sonucuna göre bir int deðeri return eder. 
+		double a = sayilar[0];
+		double b = sayilar[1];
+		double c = sayilar[2];
 
 		int koklerinSayisi = 0;
 
-		double diskriminant = Math.pow(b, 2) - 4 * a * c;
+		double diskriminant = Math.pow(b, 2) - 4 * a * c;									// Programlama alýþtýrmalarý 3.1'in çözümünden gereken hesaplamalar alýnmýþtýr. 
 
-		if (diskriminant > 0) {
+		if (diskriminant > 0) {																//Bu kýsýmda kök sayýsý bulduk ve köklerin deðerlerini gereken deðiþkenlere atadýk.
 			kokler[0] = (-b + Math.pow(diskriminant, 0.5)) / (2 * a);
 			kokler[1] = (-b - Math.pow(diskriminant, 0.5)) / (2 * a);
 			koklerinSayisi = 2;
