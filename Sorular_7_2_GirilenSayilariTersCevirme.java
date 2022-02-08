1b875af025844caa5b2bf742a736cc8f42cd50d8
muratkorkun <75913102+muratkorkun@users.noreply.github.com>
2022-02-08 18:53:20 +0300

    Güzelleştirme yapıldı.

--- Sorular_7_2_GirilenSayilariTersCevirme.java
+++ Sorular_7_2_GirilenSayilariTersCevirme.java
@@ -3,6 +3,9 @@ package Bolum07_DersteYazilanKodlar;
 import java.util.Scanner;
 
 public class Sorular_7_2_GirilenSayilariTersCevirme {
+	/*
+	 * 7.2 (Girilen sayýlarý ters çevirme) On tane tam sayý okuyan ve bu sayýlarý okuma sýrasýnýn tersi sýrada gösteren bir program yazýnýz.
+	 */
 
 	public static void main(String[] args) {
 		
@@ -10,19 +13,14 @@ public class Sorular_7_2_GirilenSayilariTersCevirme {
 		System.out.println("Lütfen ters çevrilmesi için 10 tane sayý giriniz");
 		int dizi[] = new int[10];
 		
-		for (int i = 0; i <10; i++) {
+		for (int i = 0; i <10; i++) {  // Kullanýcýdan sayýlarý tek tek alýp diziyi doldurduk.
 		dizi[i]=klavye.nextInt();
 		
 		}
-	
-		diziyiTersCevir(dizi);
-		
-		
-
+		diziyiTersCevir(dizi);       
 	}
-// 0 1 2 3 4 5 6 7 8 9 
 
-	public static void diziyiTersCevir(int[] dizi) {
+	public static void diziyiTersCevir(int[] dizi) {    // diziyi ters çevirme metodu yazdýk. Yeni bir diziye sayýlarý ters indisten tek tek atadýk.
 		int[] tersDizi=new int[10];
 		System.out.print("Girdiðiniz sayýlarýn tersten yazýlýþý : ");
 		for (int i = 0,j=9; i <10; i++,j--) {
@@ -30,4 +28,4 @@ public class Sorular_7_2_GirilenSayilariTersCevirme {
 			System.out.print(tersDizi[i]+" ");
 		}
 	}
-}
+}  // Örneklem   0 1 2 3 4 5 6 7 8 9 
